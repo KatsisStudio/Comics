f9158fa3f376affaebcc2d665b70e82d9080b414
Xwilarg <xwilarg@protonmail.com>
2024-03-28 16:39:46 +0100

    Add keyboard integration

--- js/loader.js
+++ js/loader.js
@@ -1,32 +1,58 @@
+let page;
+let img;
+
+function previousPage()
+{
+    const index = parseInt(page.dataset.page);
+    page.dataset.page = index - 1;
+    img.src = `comics/${page.dataset.comic}/pages/${index - 1}.png`;
+    window.history.pushState({}, page.dataset.name, `?comic=${page.dataset.comic}&page=${index - 1}`);
+}
+
+function nextPage()
+{
+    const index = parseInt(page.dataset.page);
+    page.dataset.page = index + 1;
+    img.src = `comics/${page.dataset.comic}/pages/${index + 1}.png`;
+    window.history.pushState({}, page.dataset.name, `?comic=${page.dataset.comic}&page=${index + 1}`);
+}
+
 window.addEventListener("load", _ => {
-    const page = document.getElementById("page");
-    const img = page.querySelector("img");
+    page = document.getElementById("page");
+    img = page.querySelector("img");
     page.addEventListener("click", (e) => {
         var rect = e.target.getBoundingClientRect();
         var x = e.clientX - rect.left;
 
         const index = parseInt(page.dataset.page);
 
-        console.log(x > rect.width / 2);
-        if (x > rect.width / 2 && index < parseInt(page.dataset.count))
+        if (x < rect.width && index > 1)
         {
-            page.dataset.page = index + 1;
-            img.src = `comics/${page.dataset.comic}/pages/${index + 1}.png`;
-            window.history.pushState({}, page.dataset.name, `?comic=${page.dataset.comic}&page=${index + 1}`);
+            previousPage();
         }
-        else if (x < rect.width && index > 1)
+        else if (x > rect.width / 2 && index < parseInt(page.dataset.count))
         {
-            page.dataset.page = index - 1;
-            img.src = `comics/${page.dataset.comic}/pages/${index - 1}.png`;
-            window.history.pushState({}, page.dataset.name, `?comic=${page.dataset.comic}&page=${index - 1}`);
+            nextPage();
         }
     });
 });
 
-window.addEventListener("popstate", _ => {
-    const page = document.getElementById("page");
-    const img = page.querySelector("img");
+window.addEventListener("keyup", e => {
+    if (e.key == "ArrowLeft") {
+        const index = parseInt(page.dataset.page);
+        if (index > 1) {
+            previousPage();
+        }
+    }
+    else if (e.key == "ArrowRight") {
+        const index = parseInt(page.dataset.page);
+        if (index < parseInt(page.dataset.count)) {
+            nextPage();
+        }
+    }
+});
 
+window.addEventListener("popstate", _ => {
     const urlParams = new URLSearchParams(window.location.search);
     const pageParam = urlParams.get('page');
 
