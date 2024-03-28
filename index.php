8bc4d43a787bd5caae8b256f94bc89b3ca0f2296
Xwilarg <xwilarg@protonmail.com>
2024-03-28 15:46:34 +0100

    Add basic page loading

--- index.php
+++ index.php
@@ -9,6 +9,32 @@ use Twig\Environment;
 $loader = new FilesystemLoader(["templates"]);
 $twig = new Environment($loader);
 
-echo $twig->render("index.html.twig", [
-    "articles" => array_map("parseMetadata", $files),
-]);
+if (isset($_GET["comic"]))
+{
+    $metadata = json_decode(file_get_contents("comics". $_GET["comic"] . "/info.json"), true);
+    $metadata["preview"] = "comocs/" . $_GET["comic"] . "/" . $metadata["preview"];
+
+    if (!isset($_GET["page"]))
+    {
+        echo $twig->render("redirect.html.twig", [
+            "metadata" => $metadata,
+            "target" => 'https://comics.katsis.net?comic=' . $_GET["comic"] . '&page=1'
+        ]);
+    }
+    else
+    {
+        $pages = $files = glob("articles/*", GLOB_ONLYDIR);
+        echo $twig->render("page.html.twig", [
+            "metadata" => $metadata,
+            "comic" => $_GET["comic"],
+            "page" => $_GET["page"]
+        ]);
+    }
+}
+else
+{
+    echo $twig->render("redirect.html.twig", [
+        "metadata" => null,
+        "page" => 'https://katsis.net'
+    ]);
+}
